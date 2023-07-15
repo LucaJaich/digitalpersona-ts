997e223bfe5228f528b33ea2eec3ba7a2fade409
LucaJaich <jaichencol@gmail.com>
2023-07-14 21:56:25 -0300

    refactor: got rid of wrong useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
 import { FingerprintReader, SampleFormat } from '@digitalpersona/devices';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function App() {
   const reader: FingerprintReader = new FingerprintReader();
-  const [capture, setCapture] = useState(false);
-  const [sample, setSample] = useState('');
-
-  useEffect(() => {
-    if (!capture)
-      return;
-    console.log('hey');
-    reader.onDeviceConnected = (device) => console.log("Device connected", device.deviceId);
-    reader.onSamplesAcquired = async (samples) => {
-      console.log(samples);
-      let image: string = samples.samples[0] as unknown as string;
-      image = image.replace(/_/g, '/').replace(/-/g, '+');
-      setSample(image);
+  reader.onDeviceConnected = (device) => console.log("Device connected", device.deviceId);
+  reader.onSamplesAcquired = async (samples) => {
+    console.log(samples);
+    console.log(typeof samples.samples[0]);
+    let image: string = samples.samples[0].toString();
+    image = image.replace(/_/g, '/').replace(/-/g, '+'); // invalid chars
+    setSample(image);
   }
 
+  const startCapture = () => {
     reader.startAcquisition(SampleFormat.PngImage, "79393E80-F896-A041-9CAD-FE9C993552F2")
     .then((res) => {
         console.log(res);
     })
     .catch((err) => console.log(err));
+  }
 
-  }, [capture]);
+  const [sample, setSample] = useState('');
 
   return (
     <div className="App">
-      <button onClick={() => setCapture(true)}>Capture</button>
+      <button onClick={startCapture}>Capture</button>
       <img src={`data:image/png;base64,${sample}`} alt="fingerprint" />
       <p>hola</p>
     </div>
